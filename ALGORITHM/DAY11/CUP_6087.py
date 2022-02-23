f7a7187a0569a2d942a6e28075aa6cd26d4e8981
psrom <psrom721@gmail.com>
2022-02-24 00:06:13 +0900

    delete

--- ALGORITHM/DAY11/CUP_6087.py
+++
@@ -1,6 +0,0 @@
-# 3의 배수 제외하고 출력
-n = int(input())
-for i in range(1, n+1):
-    if i%3==0:
-        continue
-    print(i, end=' ')
--- ALGORITHM/DAY11/CUP_6088.py
+++
@@ -1,4 +0,0 @@
-# 등차수열
-a, d, n = map(int, input().split())
-calc = a + d * (n-1)
-print(calc)
