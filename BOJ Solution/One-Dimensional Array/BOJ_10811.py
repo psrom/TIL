fa7f49a23b1d47b1386dc3f92475c4a7356446cb
psrom <psrom721@gmail.com>
2023-03-21 17:44:11 +0900

    BOJ_10811 바구니 뒤집기 풀이

--- BOJ Solution/One-Dimensional Array/BOJ_10811.py
+++ BOJ Solution/One-Dimensional Array/BOJ_10811.py
@@ -3,3 +3,19 @@
 # N: 바구니 개수, M번 바구니 순서 역순
 # i, j: i부터 j까지 역순
 
+N, M = map(int, input().split())
+basket = [(i+1) for i in range(N)]
+
+for test_case in range(M):
+    i, j = map(int, input().split())
+    if i-1 == 0:
+        basket[i-1:j] = basket[j-1::-1]
+    else:
+        basket[i-1:j] = basket[j-1:i-2:-1]
+
+print(*basket) # *를 쓰면 원소 출력 가능
+
+
+
+
+
