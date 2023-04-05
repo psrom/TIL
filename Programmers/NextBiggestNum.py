b984bedc4a8826822fc9ec14bade38eb4feee664
psrom <psrom721@gmail.com>
2023-04-05 22:54:57 +0900

    solve binary str problem

---
+++ Programmers/NextBiggestNum.py
@@ -0,0 +1,15 @@
+def solution(n):
+    bin = format(n, 'b')
+    bin = str(bin)
+    cnt = bin.count("1")
+
+    for i in range(n + 1, 1000001):
+        num = format(i, 'b')
+        num_cnt = num.count("1")
+        if num_cnt == cnt:
+            answer = i
+            break
+        else:
+            continue
+
+    return answer
