0d9f34f6653be7cef3778fc60d15b2fa742cb92e
psrom <psrom721@gmail.com>
2023-05-02 18:13:41 +0900

    SpringBoot 실습(Header, Footer 만들기까지)

---
+++ Back/SpringBoot/firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,21 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username", "hong");
+        return "greetings"; //templates/greetings.mustache -> 브라우저로 전송!
+    }
+
+    @GetMapping("/bye")
+    public String seeYouAgain(Model model){
+        model.addAttribute("nickname", "zzang");
+        return "goodbye";
+    }
+}
