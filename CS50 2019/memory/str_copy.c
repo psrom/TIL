352a8e9fa768b5f5fff5d0b0c89b4ea331baaa13
psrom <psrom721@gmail.com>
2023-05-22 18:03:40 +0900

    how to copy string

---
+++ CS50 2019/memory/str_copy.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char *s = get_string("s: ");
+    char *t = malloc(strlen(s)+1); // memory allocation
+
+    // string t = s; // address of s
+
+    strcpy(t, s); // string copy
+    // for (int i=0; n = strlen(s); i < n + 1; i++)
+    // {
+    //    t[i] = s[i];
+    // }
+
+    t[0] = toupper(t[0]);
+
+    printf("%s\n", s);
+    printf("%s\n", t);
+}
