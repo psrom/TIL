dbae8108edd4c0d424a2372ba804d4613da1f50b
psrom <psrom721@gmail.com>
2023-05-22 17:40:50 +0900

    solve the problem: comparing strings

--- CS50 2019/memory/str_comparison.c
+++ CS50 2019/memory/str_comparison.c
@@ -7,7 +7,7 @@ int main(void)
     string s = get_string("s: ");
     string t = get_string("t: ");
 
-    if (s==t)
+    if (s==t) // Different
     {
         printf("Same\n");
     }
@@ -18,3 +18,6 @@ int main(void)
 }
 // 같은 문자열을 입력해도 주소가 다르기 때문에 다른 문자열이라고 결과가 나온다.
 // 이를 어떻게 해결해야 할까?
+
+// if (*s == *t) : 주소가 아닌, 주소 안에 있는 값을 비교
+// 이 때는 Same이라고 올바른 결과가 나옴
