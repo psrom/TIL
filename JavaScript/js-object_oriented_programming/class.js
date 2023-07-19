efd25bac7d5919516e7367213cab125f898cc02e
psrom <psrom721@gmail.com>
2023-07-19 19:32:41 +0900

    ADD: use of 'super' keyword

--- JavaScript/js-object_oriented_programming/class.js
+++ JavaScript/js-object_oriented_programming/class.js
@@ -5,15 +5,22 @@ class Person{
         this.second = second;
     }
     sum(){
-        return 'prototype: '+(this.first+this.second);
+        return this.first+this.second;
     }
 }
 class PersonPlus extends Person{
+    constructor(name, first, second, third){
+        super(name, first, second);
+        this.third = third;
+    }
+    sum(){
+        return super.sum() + this.third;
+    }
     avg(){
-        return (this.first + this.second) / 2;
+        return (super.sum() + this.third) / 3;
     }
 }
 
-var kim = new PersonPlus('kim', 10, 20);
+var kim = new PersonPlus('kim', 10, 20, 30);
 console.log("kim.sum()", kim.sum());
 console.log("kim.avg()", kim.avg());
