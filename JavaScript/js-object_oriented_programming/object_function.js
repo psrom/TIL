6bbefb50de8c9b929ab9d553fe99b07d8fb38bd0
psrom <psrom721@gmail.com>
2023-07-21 19:20:43 +0900

    ADD: relation of object and function

---
+++ JavaScript/js-object_oriented_programming/object_function.js
@@ -0,0 +1,10 @@
+var kim = {name:'kim', first:10, second:20}
+var lee = {name:'lee', first:10, second:10}
+function sum(prefix){
+    return prefix + (this.first + this.second);
+}
+// sum();
+
+console.log("sum.call(kim): ", sum.call(kim, '=>'));
+console.log("sum.call(lee): ", sum.call(lee, ': '));
+
