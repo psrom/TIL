ad759c687de4127eaa05d60e6c76f8f686b4c9bb
psrom <psrom721@gmail.com>
2023-07-27 19:31:36 +0900

    ADD: return result

--- JavaScript/calculator/app.js
+++ JavaScript/calculator/app.js
@@ -1,18 +1,27 @@
 const calculator = {
-    add: function(a, b){
-        console.log(a + b);
+    plus: function(a, b){
+        return a + b;
     },
     minus: function(a, b){
-        console.log(a - b);
+        return a - b;
     },
     divide: function(a, b){
         if (b == 0){
-            console.log("b cannot be 0");
+            return "b cannot be 0";
         } else{
-            console.log(a/b);
+            return a / b;
         }
     },
-    powerof: function(a, b){
-        console.log(a ** b);
+    times: function(a, b){
+        return a * b;
+    },
+    power: function(a, b){
+        return a ** b;
     }
 };
+
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult =  calculator.power(divideResult, minusResult);
