86d24c11f08621f00266bed0ccc1bfc46d7449cf
psrom <psrom721@gmail.com>
2023-08-24 20:06:31 +0900

    ADD: controller to find all members' data

--- Back/SpringFramework/SpringFramework/src/main/java/com/psrom/registration/controller/MemberController.java
+++ Back/SpringFramework/SpringFramework/src/main/java/com/psrom/registration/controller/MemberController.java
@@ -4,12 +4,14 @@ import com.psrom.registration.dto.MemberDTO;
 import com.psrom.registration.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequestMapping("/member")  // 공통주소
@@ -46,5 +48,12 @@ public class MemberController {
         } else {
             return "login";
         }
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        model.addAttribute("memberList", memberDTOList);
+        return "list";
     }
 }
