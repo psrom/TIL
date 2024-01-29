b7652245c188fa630a19a1b88a33344e90adf427
psrom <psrom721@gmail.com>
2024-01-29 15:08:52 +0900

    feat(app): 404 not found page
    현재 코드에서는 id 값을 아무렇게나 지정해도
    404 오류 발생이 안 됨. 추후 수정 필요

--- Nodejs/main.js
+++ Nodejs/main.js
@@ -6,36 +6,40 @@ var app = http.createServer(function(request, response) {
     var myURL = new URL('http://localhost:3000' + varUrl); // http://localhost......TML
     var queryData = myURL.searchParams.get('id'); // HTML
     var title = queryData;
-    if(varUrl == '/') {
-        title = 'Welcome';
-    }
-    if(varUrl == '/favicon.ico') {
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
 
-    fs.readFile(`data/${queryData}`, 'utf8', function(err, description){
-        var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              <ul>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-              </ul>
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
-            response.end(template);
-    })
+    // console.log(myURL);
+
+    var pathname = myURL.pathname;
+
+    if(pathname === "/"){
+        fs.readFile(`data/${queryData}`, 'utf8', function(err, description){
+            var template = `
+                <!doctype html>
+                <html>
+                <head>
+                  <title>WEB1 - ${title}</title>
+                  <meta charset="utf-8">
+                </head>
+                <body>
+                  <h1><a href="/">WEB</a></h1>
+                  <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                  </ul>
+                  <h2>${title}</h2>
+                  <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+        });
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
+    }
+
 
 });
 app.listen(3000);
