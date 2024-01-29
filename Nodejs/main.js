51d6ca68c90c77aad3699b2a6f77f67cbf1771b2
psrom <psrom721@gmail.com>
2024-01-29 17:08:05 +0900

    feat(app): get filelist

--- Nodejs/main.js
+++ Nodejs/main.js
@@ -5,16 +5,24 @@ var app = http.createServer(function(request, response) {
     var varUrl = request.url; // ?id=HTML
     var myURL = new URL('http://localhost:3000' + varUrl); // http://localhost......TML
     var queryData = myURL.searchParams.get('id'); // HTML
-
     // console.log(myURL);
 
     var pathname = myURL.pathname;
 
     if(pathname === "/"){
         if(queryData === null) {
-            fs.readFile(`data/${queryData}`, 'utf8', function(err, description){
+
+            fs.readdir('./data', function(error, filelist){
                 var title = 'Welcome';
                 var description = 'Hello, Node.js';
+                var list = '<ul>';
+                var i = 0;
+                while(i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i++;
+                }
+                list = list + '</ul>';
+
                 var template = `
                     <!doctype html>
                     <html>
@@ -24,11 +32,7 @@ var app = http.createServer(function(request, response) {
                     </head>
                     <body>
                       <h1><a href="/">WEB</a></h1>
-                      <ul>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                      </ul>
+                      ${list}
                       <h2>${title}</h2>
                       <p>${description}</p>
                     </body>
@@ -36,31 +40,39 @@ var app = http.createServer(function(request, response) {
                     `;
                     response.writeHead(200);
                     response.end(template);
-            });
-        } else {
-            fs.readFile(`data/${queryData}`, 'utf8', function(err, description){
-                var title = queryData;
-                var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                      <title>WEB1 - ${title}</title>
-                      <meta charset="utf-8">
-                    </head>
-                    <body>
-                      <h1><a href="/">WEB</a></h1>
-                      <ul>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                      </ul>
-                      <h2>${title}</h2>
-                      <p>${description}</p>
-                    </body>
-                    </html>
-                    `;
-                    response.writeHead(200);
-                    response.end(template);
+                });
+
+            } else {
+                fs.readdir('./data', function(error, filelist){
+                var title = 'Welcome';
+                var description = "Hello, Node.js";
+                var list = '<ul>';
+                var i = 0;
+                while(i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i++;
+                }
+                list = list + '</ul>';
+                fs.readFile(`data/${queryData}`, 'utf8', function(err, description){
+                    var title = queryData;
+                    var template = `
+                        <!doctype html>
+                        <html>
+                        <head>
+                          <title>WEB1 - ${title}</title>
+                          <meta charset="utf-8">
+                        </head>
+                        <body>
+                          <h1><a href="/">WEB</a></h1>
+                          ${list}
+                          <h2>${title}</h2>
+                          <p>${description}</p>
+                        </body>
+                        </html>
+                        `;
+                        response.writeHead(200);
+                        response.end(template);
+                });
             });
         }
     } else {
