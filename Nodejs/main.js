d15a5c7803eaef04a1ac828945f8383024153019
psrom <psrom721@gmail.com>
2024-02-16 19:34:05 +0900

    feat(app): enhance security features

--- Nodejs/main.js
+++ Nodejs/main.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var template = require('./lib/template.js');
+var path = require('path')
+const sanitizeHtml = require('sanitize-html');
 
 var app = http.createServer(function(request, response) {
     var varUrl = request.url; // ?id=HTML
@@ -26,15 +28,18 @@ var app = http.createServer(function(request, response) {
                 })
             } else {
                 fs.readdir('./data', function(error, filelist){
-                    fs.readFile(`data/${queryData}`, 'utf8', function(err, description){
+                    var filteredId= path.parse(queryData).base;
+                    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
                         var title = queryData;
+                        var sanitizedTitle = sanitizeHtml(title);
+                        var sanitizedDescription = sanitizeHtml(description);
                         var list = template.list(filelist);
-                        var html = template.html(title, list,
-                        `<h2>${title}</h2>${description}`,
+                        var html = template.html(sanitizedTitle, list,
+                        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
                         `<a href="/create">create</a>
-                        <a href="/update?id=${title}">update</a>
+                        <a href="/update?id=${sanitizedTitle}">update</a>
                         <form action="delete_process" method="post">
-                            <input type="hidden" name="id" value="${title}">
+                            <input type="hidden" name="id" value="${sanitizedTitle}">
                             <input type="submit" value="delete">
                         </form>`
                         );
@@ -77,7 +82,8 @@ var app = http.createServer(function(request, response) {
         });
     } else if(pathname === '/update'){
         fs.readdir('./data', function(error, filelist){
-            fs.readFile(`data/${queryData}`, 'utf8', function(err, description){
+            var filteredId= path.parse(queryData).base;
+            fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
                 var title = queryData;
                 var list = template.list(filelist);
                 var html = template.html(title, list,
@@ -126,11 +132,11 @@ var app = http.createServer(function(request, response) {
               request.on('end', function(){
                   var post = qs.parse(body);
                   var id = post.id;
-                  fs.unlink(`data/${id}`, function(error){
+                  var filteredId= path.parse(id).base;
+                  fs.unlink(`data/${filteredId}`, function(error){
                     response.writeHead(302, {Location: `/`});
                     response.end();
                   })
-
               });
 
           }else {
